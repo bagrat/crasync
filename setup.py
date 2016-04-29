90eec61a0032bc43896e2e635bfc6890a9dc2fa7
Bagrat Aznauryan <bagrat@aznauryan.org>
2016-04-29 08:56:17 +0400

    Add multimple as a requirement

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import os
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+requirements = os.path.join(here, 'requirements.txt')
+
+reqs = []
+with open(requirements) as f:
+    reqs = f.readlines()
 
 
 setup(name='crasync',
@@ -6,4 +14,5 @@ setup(name='crasync',
       author='Bagrat Aznauryan',
       author_email='bagrat@aznauryan.org',
       packages=find_packages(),
+      install_requires=reqs,
       version='0.0')
