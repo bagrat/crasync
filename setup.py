e848a995a1092e10043de786e4874baf09bba9d0
Bagrat Aznauryan <bagrat@aznauryan.org>
2016-04-29 08:47:04 +0400

    Make crasync a package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(name='crasync',
+      description='Use Python 3 async methods interactively',
+      author='Bagrat Aznauryan',
+      author_email='bagrat@aznauryan.org',
+      packages=find_packages(),
+      version='0.0')
